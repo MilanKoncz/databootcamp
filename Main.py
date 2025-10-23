ba08ef4ab5d1429ec313363622b1af1d0bfb1cc6
MilanKoncz <milan.koncz1@gmail.com>
2025-10-23 20:59:03 +0200

    grr

--- Main.py
+++ Main.py
@@ -1,1 +1,14 @@
-print("AA")
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import warnings
+import matplotlib.pyplot as plt
+from scipy import stats
+
+warnings.filterwarnings("ignore")
+
+#Relativer Pfad geht nicht :(
+df_crimes = pd.read_csv("/Users/milan/Desktop/Coding/Datacamp/databootcamp/data/Crimes_Dataset.csv")
+df_sus = pd.read_csv("/Users/milan/Desktop/Coding/Datacamp/databootcamp/data/Suspects_Dataset.csv")
+
+df_crimes.describe()
